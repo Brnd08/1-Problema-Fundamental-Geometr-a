cfcd04a0c65c35c1fca5b79507b564fcce90ba2e
Brnd <bvelazquez5605@gmail.com>
2021-10-27 22:08:32 -0500

    ProblemaFundamental Version 1.2 Modificacion en el proceso de simplificacion de la ecuacion (Se cambio la iteracion por recursividad un metodo mas limpio y rapido)

--- src/problema_fundamental/EntradaEcuacion.java
+++ src/problema_fundamental/EntradaEcuacion.java
@@ -33,51 +33,14 @@ public class EntradaEcuacion {
     }
     
     public void simplificarEcuacion(){
-        int a=Math.abs(ecuacion[0]);//abs x
-        int b=Math.abs(ecuacion[1]);//abs y
-        int c=Math.abs(ecuacion[2]);//abs c
-        int menor1 = a;
-        int mayor1 = a;
-        
-        for (int i = 1; i < 2; i++) { //Encuentre el valor minimo, maximo de los valores absolutos de la ecuacion
-            if(menor1 == 0)
-                menor1=Math.abs(ecuacion[i]);
-                
-            if( Math.abs(ecuacion[i]) < menor1 & ecuacion[i]!=0 )     
-                menor1=Math.abs(ecuacion[i]);                
-            
-            if( Math.abs(ecuacion[i]) > mayor1)
-                mayor1=Math.abs(ecuacion[i]);
-            }
-        
-        int mcdAb=menor1;        
-        
-        while( mayor1%menor1!=0){//calcula el MCD de A y B  partir de el Algoritmo de Euclides
-            menor1= mayor1%menor1;
-            mcdAb=menor1;            
-            mayor1=mcdAb;            
-        }
-        
-        int mcdFinal=mcdAb;
-        if (c>0){   //si c = 0 se queda 
-            
-            int menor2 = Math.min(mcdAb, c);
-            int mayor2 = Math.max(mcdAb, c);            
-            
-            int mcdC=menor2;
-            
-            while( mayor2%menor2!=0){//calcula el MCD de MCD (a,b) y C  partir de el Algoritmo de Euclides
-                menor2= mayor2%menor2;
-                mcdC=menor2;            
-                mayor2=mcdC;            
-            }
-            mcdFinal=mcdC;
-        }                
-        
-        System.out.printf("%36s\u2193\u2193\u2193\n", "Simplificando tenemos  ");
+        int a = ecuacion [0];
+        int b = ecuacion [1];
+        int c = ecuacion [2];
+        int mcd =Mcd_Recursivo.mcd(a, b, c);
         for (int k = 0; k < 3; k++) // AQUI SE DIVIDE ENTRE EL MCD DE LA ECUACION
-            ecuacion[k] = ecuacion[k]/mcdFinal; 
+            ecuacion[k] = ecuacion[k]/mcd; 
         
+        System.out.printf("%36s\u2193\u2193\u2193\n", "Simplificando al dividir entre " + mcd +  "tenemos  ");
       
     }
                                                         //    vX                "x"           vY                   "y"       vI                   
