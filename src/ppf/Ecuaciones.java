0efcc8b5392583f69dde8029a934caea2ef43627
Brdn V.T <bvelazquez5605@gmail.com>
2021-11-26 22:05:13 -0600

    Proyecto (version 1.5) con usuarios, redimensionado JMenu y Simplificar

---
+++ src/ppf/Ecuaciones.java
@@ -0,0 +1,139 @@
+package ppf;
+
+import java.io.Serializable;
+import java.util.Arrays;
+
+public class Ecuaciones implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String expresion;
+    private String[] Despejes, intervalos, simetria, interceptos, asintotas;
+
+    public Ecuaciones(String expresion, String[] Despejes, String[] intervalos, String[] simetria, String[] interceptos, String[] asintotas) {
+        this.expresion = expresion;
+        this.Despejes = Despejes;
+        this.intervalos = intervalos;
+        this.simetria = simetria;
+        this.interceptos = interceptos;
+        this.asintotas = asintotas;
+    }
+
+    public Ecuaciones() {
+        this.expresion = "";
+        this.Despejes = new String[2];
+        this.intervalos = new String[2];
+        this.simetria = new String[3];
+        this.interceptos = new String[2];
+        this.asintotas = new String[2];
+    }
+
+    public void setExpresion(String expresion) {
+        this.expresion = expresion;
+    }
+
+    public void setDespejeX(String DespejeX) {
+        this.Despejes[0] = DespejeX;
+    }
+
+    public void setDespejeY(String DespejeY) {
+        this.Despejes[1] = DespejeY;
+    }
+
+    public void setIntervaloX(String intervaloX) {
+        this.intervalos[0] = intervaloX;
+    }
+
+    public void setIntervaloY(String intervaloY) {
+        this.intervalos[1] = intervaloY;
+    }
+
+    public void setSimetriaX(String simetriaX) {
+        this.simetria[0] = simetriaX;
+    }
+
+    public void setSimetriaY(String simetriaY) {
+        this.simetria[1] = simetriaY;
+    }
+
+    public void setSimetriaO(String simetriaY) {
+        this.simetria[2] = simetriaY;
+    }
+
+    public void setInterceptoX(String interceptoX) {
+        this.interceptos[0] = interceptoX;
+    }
+
+    public void setInterceptoY(String interceptoY) {
+        this.interceptos[1] = interceptoY;
+    }
+
+    public void setAsintotaH(String asintotaH) {
+        this.asintotas[0] = asintotaH;
+    }
+
+    public void setAsintotaV(String asintotaV) {
+        this.asintotas[1] = asintotaV;
+    }
+
+    public String getExpresion() {
+        return this.expresion;
+    }
+
+    public String getDespejeX() {
+        return this.Despejes[0];
+    }
+
+    public String getDespejeY() {
+        return this.Despejes[1];
+    }
+
+    public String getIntervaloX() {
+        return this.intervalos[0];
+    }
+
+    public String getIntervaloY() {
+        return this.intervalos[1];
+    }
+
+    public String getSimetriaX() {
+        return this.simetria[0];
+    }
+
+    public String getSimetriaY() {
+        return this.simetria[1];
+    }
+
+    public String getSimetriaO() {
+        return this.simetria[2];
+    }
+
+    public String getInterceptoX() {
+        return this.interceptos[0];
+    }
+
+    public String getInterceptoY() {
+        return this.interceptos[1];
+    }
+
+    public String getAsintotaH() {
+        return this.asintotas[0];
+    }
+
+    public String getAsintotaV() {
+        return this.asintotas[1];
+    }
+
+    @Override
+    public String toString() {
+        String representacion = "[" + " expresion-> " + this.expresion
+                + " Despejes-> " + Arrays.toString(Despejes)
+                + " Intervalos-> " + Arrays.toString(intervalos)
+                + " Simetrias-> " + Arrays.toString(simetria)
+                + " Interceptos-> " + Arrays.toString(interceptos)
+                + " Asintotas-> " + Arrays.toString(asintotas)
+                + "]";
+        return representacion;
+    }
+
+}
---
+++ src/ppf/Paso_3.java
@@ -0,0 +1,85 @@
+package ppf;
+
+import static java.lang.Double.isInfinite;
+
+
+public class Paso_3 {
+        
+    private int ecuacion[] = new int[3];
+    private float pendiente;
+    private String variables[] = new String[3];
+    
+
+    Paso_3(int[] ecuacion, String[] variables) {
+        this.ecuacion = ecuacion;
+        this.variables = variables;
+        this.pendiente = this.calcularPendiente(ecuacion);
+        
+        System.out.println("\n\n 3)  SIMETRIAS:");
+    }
+        
+     private float calcularPendiente(int [] ecuacion2){
+        /*
+         * pendiente = - a/b
+         */
+        
+        int b = ecuacion2[1];
+        int a = ecuacion2[0];
+        
+        String sPendiente = (-a)+ "/" + b + " = ";
+        float pendiente2 ;
+        
+        try{
+            pendiente2 = (float) -a/b;
+            System.out.println(" Pendiente:\t" + sPendiente +  pendiente2);
+        }
+        catch(Exception e){
+            pendiente2 = (float) -a/b;
+            System.out.println(" Pendiente:\t" + sPendiente +  pendiente2);
+        }
+         return pendiente2;
+    }
+     
+    public String[] simetrias() {
+        String simetriasS[] = {"default X", "default Y", "default O"};
+
+        if ( this.esSimetricaX() )
+                simetriasS[0] = "Simetria respecto en eje X";
+            else 
+                simetriasS[0] = "Sin simetria en eje X";
+
+        if ( this.esSimetricaY() )
+                simetriasS[1] =String.format("Simetria en eje Y");
+            else 
+                simetriasS[1] =String.format("Sin simetria en eje Y");
+
+        if ( this.esSimetricaOrigen() )
+                simetriasS[2] =String.format("Simetria en Origen");
+            else 
+                simetriasS[2] =String.format("SIn simetria en Origen");   
+
+        System.out.print( "\n Eje X \t" + simetriasS[0] );
+        System.out.print( "\n Eje Y \t" + simetriasS[1] );
+        System.out.print( "\n Origen Y \t" + simetriasS[2] );
+                
+        return simetriasS.clone();
+    }
+
+    private  boolean  esSimetricaX (){
+        return isInfinite(pendiente);
+    }
+
+    private  boolean esSimetricaY (){
+        return  (pendiente == 0.0f ); 
+    }
+
+    private  boolean esSimetricaOrigen (){
+        return ecuacion[2] == 0;
+    }
+
+    public float getPendiente() {
+        return pendiente;
+    }
+        
+     
+}
